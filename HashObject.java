9a0fd15d1bf5d6b82adfbf04c8217c19dd5f5e62
Matt Youngberg <matt_youngberg@outlook.com>
2023-10-12 13:24:41 -0600

    Cleaned up Javadocs

--- HashObject.java
+++ HashObject.java
@@ -1,6 +1,16 @@
 /**
  * An object that can be used as a key in a hash table.
  * 
+ * A HashObject additionally keeps track of two counts:
+ * 
+ * 1) The frequency which a given key was attempted to be inserted by calling code (namely, the 
+ * Hashtable). For convenience, this is initialized to 1 assuming that a HashObject is only 
+ * constructed when it is ready for insertion.
+ * 
+ * 2) The number of probes required to initially insert this key. This is expected to be set by the
+ * Hashtable, which is aware of how many items of the probe sequence has to be queried within the
+ * table before finding an unoccupied insertion point.
+ * 
  * @author Matt Youngberg
  */
 public class HashObject {
@@ -11,17 +21,25 @@ public class HashObject {
     private Object key;
 
     /**
-     * The amount of duplicates counted.
+     * The count of insertions of this key, including the initial one.
      */
     private int frequencyCount;
 
     /**
-     * The amount of times the hash table was probed.
+     * The number of probes required to initially insert this key.
+     * 
+     * This is not affected by duplicate insertions of the same key. In that sense, once set by the
+     * method setProbeCount, this value should be traeted as immutable.
      */
     private int probeCount;
 
     /**
      * Constructs a new HashObject with the given key.
+     * 
+     * Note: Frequency count is initialized to 1 to count this instance. Probe count is initialized
+     * to 0 and is expected to be called by the Hashtable, which is aware of how many items of the
+     * probe sequence has to be queried within the table before finding an unoccupied insertion 
+     * point.
      * 
      * @param key The key of this HashObject
      */
@@ -41,30 +59,39 @@ public class HashObject {
     }
 
     /**
-     * TODO
+     * Increment the frequency count of this HashObject.
+     * 
+     * Should be called by the hash table when a duplicate of a key it's trying to insert is found.
      */
     public void incrementFrequency() {
         this.frequencyCount++;
     }
     
     /**
-     * TODO
-     * @return
+     * Retrieves the frequency of insertion attempts for the key represented by this HashObject.
+     * 
+     * @return The frequency of insertion attempts for the key represented by this HashObject
      */
     public int getFrequencyCount() {
         return frequencyCount;
     }
     
     /**
-     * TODO
+     * Set the probe count of this HashObject.
+     * 
+     * This should be called by the hash table when it is aware of how many items of the probe
+     * sequence has to be queried within the table before finding an unoccupied insertion point.
+     * 
+     * @param probeCount The probe count of this HashObject
      */
     public void setProbeCount(int probeCount) {
         this.probeCount = probeCount;
     }
     
     /**
-     * TODO
-     * @return
+     * Get the probe count of this HashObject.
+     * 
+     * @return The probe count of this HashObject
      */
     public int getProbeCount() {
         return probeCount;
@@ -73,8 +100,13 @@ public class HashObject {
     /**
      * Returns true if the given object is a HashObject with the same key as this one.
      * 
+     * Note: The project spec asks us to create this method, but I found its only use in the
+     * Hashtable.insert() method. Within, I could have just compared the keys directly in that
+     * method (as opposed to creating a passing reference to a new HashObject just for the sake of
+     * comparison), but the spec wanted me to use this method somewhere, so I used it there.
+     * 
      * @param obj The object to compare to this one
-     * @return    True if the given object is a HashObject with the same key as this one
+     * @return    Wehter the given object is a HashObject with the same key as this one
      */
     @Override
     public boolean equals(Object obj) {
